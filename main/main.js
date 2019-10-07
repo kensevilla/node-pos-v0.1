823e21e8aa91601e1b2e31c8fcd7142eda280289
KEN MICHAEL SEVILLA (SHP-IRIS-ISD-OOCLL/MNL) <SEVILKE@OOCL.COM>
2019-10-07 17:33:44 +0800

    fix printInventory method

--- main/main.js
+++ main/main.js
@@ -5,25 +5,38 @@ module.exports = function main(inputs) {
 function printInventory(inputs){
     var receipt;
     var currentList = [];
+    var hasList = false;
+    var totalCost = 0;
     for(var x = 0; x < inputs.length; x++){
-        if(currentList.length == 0){
-            currentList.push({
-                Barcode : x.Barcode,
-                Name : x.Name,
-                Quantity : 1,
-                Unit : x.Price,
-                Subtotal : x.Price
-            });
-        }
-        else{
-            for(var y = 0; y < currentList.length; y++){
-                if(inputs[x].Barcode == currentList[y].Barcode){
-                    currentList[y].Quantity++;
-                    currentList[y].Subtotal += inputs[x].Price;
-                }
+        hasList = false;
+        for(var y = 0; y < currentList.length; y++){
+            if(inputs[x].Barcode == currentList[y].Barcode){
+                currentList[y].Quantity++;
+                currentList[y].Subtotal += inputs[x].Price;
+                hasList = true;
+                break;
             }
         }
+        if(!hasList){
+                currentList.push({
+                    Barcode : inputs[x].Barcode,
+                    Name : inputs[x].Name,
+                    Quantity : 1,
+                    Price : inputs[x].Price,
+                    Unit : inputs[x].Unit,
+                    Subtotal : inputs[x].Price
+                });
+        }
     }
+    receipt = '***<store earning no money>Receipt ***\n';
+    currentList.forEach(item => {
+        if(item.Unit!=='bottle') receipt += 'Name: '+ item.Name + ', Quantity: '+ item.Quantity +', Unit price: '+ item.Price.toFixed(2) +' (yuan), Subtotal: '+ item.Subtotal.toFixed(2) +' (yuan)\n';
+        else receipt += 'Name: '+ item.Name + ', Quantity: '+ item.Quantity +' bottles, Unit price: '+ item.Price.toFixed(2) +' (yuan), Subtotal: '+ item.Subtotal.toFixed(2) +' (yuan)\n';
+        totalCost+=item.Subtotal;
+    });
 
+    receipt += '----------------------\n';
+    receipt += 'Total: '+ totalCost.toFixed(2) +' (yuan)\n';
+    receipt += '**********************\n';
     return receipt;
 }
