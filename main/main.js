2403b7253122033966268cc6b399c935c688d00e
KEN MICHAEL SEVILLA (SHP-IRIS-ISD-OOCLL/MNL) <SEVILKE@OOCL.COM>
2019-10-07 13:31:47 +0800

    implement printInventory method

--- main/main.js
+++ main/main.js
@@ -1,4 +1,29 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(inputs) {
+    return printInventory(inputs);
 };
+
+function printInventory(inputs){
+    var receipt;
+    var currentList = [];
+    for(var x = 0; x < inputs.length; x++){
+        if(currentList.length == 0){
+            currentList.push({
+                Barcode : x.Barcode,
+                Name : x.Name,
+                Quantity : 1,
+                Unit : x.Price,
+                Subtotal : x.Price
+            });
+        }
+        else{
+            for(var y = 0; y < currentList.length; y++){
+                if(inputs[x].Barcode == currentList[y].Barcode){
+                    currentList[y].Quantity++;
+                    currentList[y].Subtotal += inputs[x].Price;
+                }
+            }
+        }
+    }
+
+    return receipt;
+}
